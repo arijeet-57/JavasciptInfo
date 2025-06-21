57054220182834eb44d8bf3a7eaffc413441d80d
Arijeet Roy <blakelabs57@gmail.com>
2025-06-21 16:34:11 +0530

    Practice

---
+++ alert.js
@@ -0,0 +1,6 @@
+// alert("I’m JavaScript!");
+
+
+// let name = prompt('What is your name?', "yourname");
+// alert(`Your name is ${name} !`);
+
---
+++ variable.js
@@ -0,0 +1,9 @@
+// let admin;
+// let name;
+// name = "John";
+// admin = name;
+//  alert(admin)
+
+let ourPlanet = "Earth";
+let currentUserName = "Jon";
+
